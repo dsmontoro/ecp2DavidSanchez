752ed6a51e147f96ccba3c3a992d782c0a09dca2
jherranz <jherranb@gmail.com>
2014-10-06 12:45:19 +0200

    Añadidos métodos para el test de las nuevas funcionalidades pedidas

--- src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
@@ -41,5 +41,37 @@ public class FractionTest {
 	public void testDecimal() {
 		assertEquals((double) 2, fraction.decimal());
 	}
+	
+	public void testIsPropia(){
+		fraction = new Fraction(17, 5);
+	    assertEquals(true, fraction.isPropia());
+		
+	}
+	
+	public void testIsImpropia(){
+		fraction = new Fraction(17, 5);
+	    assertEquals(true, fraction.isImpropia());
+		
+	}
+	
+	public void testIsEquivalente(){
+		fraction = new Fraction(17, 5);
+		Fraction fraction2 = new Fraction(34, 10);
+	    assertEquals(true, fraction.isEquivalente(fraction2));		
+	}
+	
+	public void testMayor(){
+		fraction = new Fraction(1, 2);
+		Fraction fraction2 = new Fraction(1, 3);
+	    assertEquals(true, fraction.mayor(fraction2));
+		
+	}
+	
+	public void testMenor(){
+		fraction = new Fraction(1, 2);
+		Fraction fraction2 = new Fraction(1, 3);
+	    assertEquals(false, fraction.menor(fraction2));
+		
+	}
 
 }
